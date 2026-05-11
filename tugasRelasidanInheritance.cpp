fcdceebb163833e8fe1e8ada923f057d3e6dac70
muhhafidzal <muh.hafidz.al.mubaroq@gmai.com>
2026-05-11 16:27:33 +0700

    membuat parent class user dengan static globalId

--- tugasRelasidanInheritance.cpp
+++ tugasRelasidanInheritance.cpp
@@ -1,3 +1,34 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+
+class User {
+protected:
+    static int globalId;
+    int id;
+    string nama;
+    string email;
+
+public:
+    User(string nama, string email) {
+        this->id = generateId();
+        this->nama = nama;
+        this->email = email;
+    }
+
+    static int generateId() {
+        return ++globalId;
+    }
+
+    int getId() {
+        return id;
+    }
+
+    string getNama() {
+        return nama;
+    }
+
+    string getEmail() {
+        return email;
+    }
+};
